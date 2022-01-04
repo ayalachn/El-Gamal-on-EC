1eb35b521b7e5178a1fee8aae88f20d0aea35da8
ayalachn <81107065+ayalachn@users.noreply.github.com>
2022-01-04 09:27:05 +0200

    Update ElGamalEcc.py

--- ElGamalEcc.py
+++ ElGamalEcc.py
@@ -10,23 +10,46 @@ algorithm, calculations based on ECC.
 @author: Ayala Cohen
 """
 
-# import ECC
 from tinyec.ec import SubGroup, Curve, Point, mod_inv, Inf
 import hashlib
 import binascii
 from random import randrange
+"""
+El-Gamal on ECC. Holds function for computing & verifying digital signatures.
+Parameter values n, G, a, b taken from ANSInet paper.
+"""
 class ElGamalEcc:
     
     prKey=0       # private key (Alice's or Bob's. Depends on who 
                   # this class is an instance of)
     myPublicK=0             # Alice's public key
     othersPublicK=0         # Bob's public key
-    field = SubGroup(p=29, g=(5, 7), n=31, h=1) # G = {15, 13}, which has order of n = 18
-    curve = Curve(a=-1, b=16, field=field, name='p1707') # y2 ≡ x3 + 7 (mod 17)
-    G = curve.g                     # G=(15,13)
-    n = 31
+    
+    """
+    Create field with given parameters:
+        G (generator) = (5,7)
+        p (Fp - field over prime p) = 29
+        n (prime order) = 31
+    """
+    field = SubGroup(p=29, g=(5, 7), n=31, h=1) # G = {5, 7}, which has order of 31
+    
+    """
+    Create Elliptic Curve y2 ≡ x3 - x + 16 (mod 29), over given field (above).
+    Curve Parameters:
+        a = -1
+        b = 16
+    """
+    curve = Curve(a=-1, b=16, field=field, name='p1707') # y2 ≡ x3 - x + 16 (mod 29)
+    G = curve.g                     # set G=(5,7) - generator of curve
+    n = 31  # THIS IS P!                         
     
     def __init__(self, prKey): # class constructor
+        """
+        Private Key must be in range [1,n-1]    
+        """
+        if prKey < 1 or prKey > self.n:
+            print("Invalid private key for El-Gamal! Key must be in range [1,",self.n,"-1]")
+            return
         self.prKey = prKey
         """
         Calculate public key using the formula:
@@ -42,25 +65,25 @@ class ElGamalEcc:
         return self.myPublicK
     
     def digitalSignMessage(self, m):
-        """ Alice signs the message:
+        """ Alice signs the message m:
             1. Create a hash of the message e=HASH(m)
+            Our hash function in SHA-256.
             """
         e = str(hashlib.sha256(m.encode('utf-8')).hexdigest())
         
-        e = str(bin(int(e, 16)))
+        e = str(bin(int(e, 16))) # Convert from hex to binary
         """
-        2. Let z be n leftmost bits of e (n=17 in our case)
+        2. Let z be n leftmost bits of e (n=31 in our case)
         """
         z = e[0:self.n]
-        z = int(z, 16)
+        z = int(z, 16)  # Convert from binary to hex
         while(True):
             """
-            3. Create a random number k which is between 1 and n-1 (n=18 => n-1=17)
+            3. Create a random number k which is between 1 and n-1 (30)
             """
-            # k = 13 # for debug
-            k = randrange(17)
+            k = randrange(self.n-1)
             """
-            4. Calculate a point of the curve as (x1,y1)=k X G
+            4. Calculate a point on the curve as (x1,y1)=k X G
             """
             point = k * self.G
             """
@@ -78,88 +101,38 @@ class ElGamalEcc:
                 break
         
         """ 
-        7. The signature is the pair (r,s)
+        7. The signature is the pair (R,s) (Point=R)
         """
-        
-        print("EL-GAMAL FINISHED SIGNATURE CREATION") #TEMP ********************************************
-        
-        # return r, s
         return point, s
 
     def verifyDigitalSignature(self, m, r, s):
+        """
+        Verify that s is an integer in [1,n-1] and R is an element in E(F_p=F_29)
+        """
+        if s < 1 or s > self.n or not self.curve.on_curve(r.x,r.y):
+            return False
         """
         Bob will check the digital signature:
         1. Create a hash of the message e=HASH(m)
         """
         e = str(hashlib.sha256(m.encode('utf-8')).hexdigest())
-        e = str(bin(int(e, 16)))
+        e = str(bin(int(e, 16))) # Convert from hex to binary
         """
-        2. z will be the n leftmost bits of e (n=17)
+        2. z will be the n leftmost bits of e (n=31)
         """
         z = e[0:self.n]
         z = int(z, 16)
         
-        #
+        """
+        3. Compute V1 = sR
+        """
         V1 = s*r
+        
+        """
+        4. Compute V2 = H(M)G+rA (where r= [x coordinate of R])
+        """
         V2 =z*self.G + r.x*self.othersPublicK
-        print("V1: ", V1,"\nV2: ", V2)
-        if (V1 == V2):
+        """ Accept iff V1 = V2 """
+        if (V1 == V2): 
             return True
         return False
-        # 
-        
-        
-        # """
-        # 3. Calculate c=s^-1 mod n
-        # """
-        # print("s=",s)
-        # inv_s = mod_inv(s, self.n) # inverse of s
-        # print("inverse of s=",inv_s)
-        # c = inv_s % self.n
-        # print("c=",c)
-        # """
-        # 4. Calculate:
-        #     u1 = z*c mod n
-        #     u2 = r*c mod n
-        # """
-        # print("z=", z)
-        # u1 = z*c % self.n
-        # u2 = r.x*c % self.n
-        # print("u1=",u1, " u2=",u2)
-        # """
-        # 5. Calculate the curve point:
-        #     (x1, y1) = u1 X G + u2 X pubK_A.
-        #     If (x1,y1)=O then the signature is invalid.
-        # """
-        # print("u1*self.G + u2*self.othersPublicK = \n",u1*self.G, "+",u2*self.othersPublicK )
-        
-        # print("Others public = ", self.othersPublicK)
-        
-        # point = u1*self.G + u2*self.othersPublicK
-        # print("point = ", point, " inf = ", Inf(curve=self.curve))
-        # if point == Inf(curve=self.curve):
-        #     print("Signature is invalid. Line 121")
-        #     return False
-        # """
-        # 6. The signature is valid if r=x1 mod n. Invalid otherwise
-        # """   
-        # print("r=",r, " x1=",point.x)
-        # if r == point.x % self.n:
-        #     print("Signature is valid.")
-        #     return True
-        # else:
-        #     print("Signature is invalid. Line 130")
-        #     return False
-
-# bob = ElGamalEcc(17)
-# alice = ElGamalEcc(23)      
- 
-# print("==== SIGN MESSAGE ====")  
-# alice.setOthersPublicKey(bob.getMyPublicKey())
-# msg="33"   
-# r,s = alice.digitalSignMessage(msg)
-# print("r=",r," s=",s)
-
-# print("==== VERIFY MESSAGE ====")  
-# bob.setOthersPublicKey(alice.getMyPublicKey()) 
-# print(bob.verifyDigitalSignature(m=msg, r=r, s=s))
